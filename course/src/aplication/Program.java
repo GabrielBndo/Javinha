db84e4d3ffba903aa006a97ea3d07c0b71024c04
gabrielbndo <gabriel.bernardo.cruuz@gmail.com>
2024-01-16 20:09:51 -0300

    finalizado

--- course/src/aplication/Program.java
+++ course/src/aplication/Program.java
@@ -4,10 +4,26 @@
  */
 package aplication;
 
-/**
- *
- * @author gabri
- */
+import entities.Products;
+import java.util.Locale;
+import java.util.Scanner;
 public class Program {
-    
+    public static void main(String[] args) {
+        
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        
+        Products products = new Products();
+        System.out.println("Enter product data:");
+        System.out.print("Name: ");
+        products.name = sc.nextLine();
+        System.out.print("Price: ");
+        products.price = sc.nextDouble();
+        System.out.print("Quantity: ");
+        products.quantity = sc.nextInt();
+        
+        System.out.println(products.name + ", " + products.price + ", " + products.quantity);
+        
+        sc.close();
+    }
 }
