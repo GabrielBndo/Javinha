7a56bd5c02e675e31bf5a95b7251c8ebeafad0e3
gabrielbndo <gabriel.bernardo.cruuz@gmail.com>
2024-01-16 19:53:11 -0300

    projetin products

---
+++ course/src/entities/Products.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entities;
+
+/**
+ *
+ * @author gabri
+ */
+public class Products {
+        public String name;
+        public double price;
+        public int quantity;
+        
+        public double totalValueInStock(){
+            return price * quantity; 
+        }
+        
+        public void addProducts(int quantity){
+            vasco da gama 
+}
+}
